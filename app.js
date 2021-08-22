c2965c36286e40804c7bfa79fb241648845a5f33
kelechi prosper <prosperkelechi50@gmail.com>
2021-08-22 13:56:06 +0100

    i

---
+++ app.js
@@ -0,0 +1,48 @@
+// // log to console
+console.log('Hello World');
+console.log(123);
+console.log(true);
+var greeting = 'Hello';
+console.log(greeting);
+console.log([1,2,3,4]);
+console.log({a:1, b:2});
+console.table({a:1, b:2});
+
+
+console.time('Hello World');
+console.log('Hello World');
+console.log('Hello World');
+console.log('Hello World');
+console.log('Hello World');
+console.log('Hello World');
+console.timeEnd('Hello World');
+
+
+// var,let,const
+
+// var name = 'John Doe';
+// console.log(name);
+// name = 'Steve smith';
+// console.log(name);
+
+// // Init var
+
+// var greeting;
+// console.log(greeting);
+// greeting = 'Hello';
+// console.log(greeting);
+
+// // letters,numbers,_,,$
+// // Can not start with number
+
+// // Multi word vars
+// var firstName = 'John'; // camel case
+// var first_name = 'sara'; // Underscore
+// var FirstName = 'Tom';// Pascal case
+
+// LET
+let name
+name = 'John Doe';
+console.log(name);
+name = 'Steve smith';
+console.log(name);
