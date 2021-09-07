468eaeff4d05bfaae7abee75b37a22508eab934f
kelechi prosper <prosperkelechi50@gmail.com>
2021-09-07 07:19:15 +0100

    i

--- app.js
+++ app.js
@@ -373,7 +373,7 @@
 //     do something else
 // }
 
-const id = '100';
+const id = 100;
 
 // // EQUAL TO
 // if(id == 100){
@@ -418,12 +418,41 @@ const id = '100';
 
 // IF ELSE
 
-const color = 'yellow';
+//const color = 'yellow';
 
-if(color ===  'red'){
-    console.log('color is red');
-} else if(color == 'blue'){
-    console.log('color is blue');
+// if(color ===  'red'){
+//     console.log('color is red');
+// } else if(color == 'blue'){
+//     console.log('color is blue');
+// } else {
+//     console.log('color is not red or blue');
+// }
+
+// LOGICAL OPERATORS
+
+const name = 'steve';
+const age = 70;
+
+// AND &&
+if(age > 0 && age < 12){
+    console.log(`${name} is a child`);
+} else if (age >= 13 &&age <= 19){
+    console.log(`${name} is a teenager`);
 } else {
-    console.log('color is not red or blue');
+    console.log(`${name} is an adult`);
 }
+// OR ||
+if(age < 16 || age > 65){
+    console.log(`${name} can not run in a race`);
+} else {
+    console.log(`${name}is registere for the race`);
+}
+
+// TERNERY OPERATOR
+console.log(id === 100 ? 'CORRECT' : 'INCORRECT');
+
+// WITHOUT BRACES
+if(id === 100)
+    console.log('CORRECT');
+else
+    console.log('INCORRECT');
