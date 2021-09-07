607f7ecfc72dd31b54ca62bc9b0bbbaaa4e4e841
kelechi prosper <prosperkelechi50@gmail.com>
2021-09-07 08:40:59 +0100

    i

--- app.js
+++ app.js
@@ -457,52 +457,57 @@
 // else
 //     console.log('INCORRECT');
 
-const color = 'yellow';
+// const color = 'yellow';
 
-switch(color){
-    case 'red':
-        console.log('color is red');
-        break;
-        case 'blue':
-        console.log('color is blue');
-        break;
-    default:
-        console.log('color is not red or blue');
-        break;
-}
+// switch(color){
+//     case 'red':
+//         console.log('color is red');
+//         break;
+//         case 'blue':
+//         console.log('color is blue');
+//         break;
+//     default:
+//         console.log('color is not red or blue');
+//         break;
+// }
 
-let day;
+// let day;
 
-switch(new Date().getDay()){
-    case 0:
-        day = 'sunday';
-        break;
+// switch(new Date().getDay()){
+//     case 0:
+//         day = 'sunday';
+//         break;
         
-        case 1:
-        day = 'monday';
-        break;
+//         case 1:
+//         day = 'monday';
+//         break;
 
-        case 2:
-        day = 'tuesday';
-        break;
+//         case 2:
+//         day = 'tuesday';
+//         break;
 
-        case 3:
-        day = 'wednesday';
-        break;
+//         case 3:
+//         day = 'wednesday';
+//         break;
 
-        case 4:
-        day = 'thursday';
-        break;
+//         case 4:
+//         day = 'thursday';
+//         break;
 
-        case 5:
-        day = 'friday';
-        break;
+//         case 5:
+//         day = 'friday';
+//         break;
 
-        case 6:
-        day = 'saturday';
-        break;
+//         case 6:
+//         day = 'saturday';
+//         break;
+// }
+
+// console.log(`today is ${day}`);
+
+// FUNCTION DECLARATION
+function greet(firstName){
+    //console.log('Hello');
+    return 'Hello ' + firstName + ' ' + lastName;
 }
-
-console.log(`today is ${day}`);
-
-
+console.log(greet('John', 'Doe'));
