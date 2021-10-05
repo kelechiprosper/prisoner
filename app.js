bfccfb91eba83301c8cd363c8d77170bbf19dd80
kelechi prosper <prosperkelechi50@gmail.com>
2021-10-05 02:14:27 +0100

    i

--- app.js
+++ app.js
@@ -790,8 +790,20 @@ let val;
 
 // document.getElementsByClassName
 
-const items = document.getElementsByClassName('collection-item');
-console.log(items);
-console.log(items[0]);
-items[0].style.color = 'blue';
-items[3].textContent = 'chief';
+// const items = document.getElementsByClassName('collection-item');
+// console.log(items);
+// console.log(items[0]);
+// items[0].style.color = 'blue';
+// items[3].textContent = 'chief';
+
+// const listItems = document.querySelector('ul').getElementsByClassName
+// ('collection-item');
+
+// console.log(listItems);
+
+// document.getElementsByClassName
+const lis = document.getElementsByTagName('li');
+console.log(lis);
+console.log(lis[0]);
+lis[0].style.color = 'red';
+lis[3].textContent = 'chief';
