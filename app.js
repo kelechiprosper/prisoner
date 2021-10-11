6b7733451e892290988fe2ee446284bc77799c8a
kelechi prosper <prosperkelechi50@gmail.com>
2021-10-11 08:29:13 +0100

    i

--- app.js
+++ app.js
@@ -933,50 +933,67 @@ let val;
 
 // REPLACE ELEMENT
 
-// Creat Element
-const newHeading = document.createElement('h2');
-// Add id
-newHeading.id = 'task-title';
-// New text node
-newHeading.appendChild(document.createTextNode('Task List'));
+// // Creat Element
+// const newHeading = document.createElement('h2');
+// // Add id
+// newHeading.id = 'task-title';
+// // New text node
+// newHeading.appendChild(document.createTextNode('Task List'));
 
-// Get the Old Heading
-const oldHeading = document.getElementById('task-title');
-//Parent
-const cardAction = document.querySelector('.card-action');
+// // Get the Old Heading
+// const oldHeading = document.getElementById('task-title');
+// //Parent
+// const cardAction = document.querySelector('.card-action');
 
-// Replace
-cardAction.replaceChild(newHeading,oldHeading);
+// // Replace
+// cardAction.replaceChild(newHeading,oldHeading);
 
-// Remove Element
-const lis = document.querySelectorAll('li');
-const list = document.querySelector('ul');
+// // Remove Element
+// const lis = document.querySelectorAll('li');
+// const list = document.querySelector('ul');
 
-// Remove list item
-lis[0].remove();
+// // Remove list item
+// lis[0].remove();
 
-// Remove child Element
-list.removeChild(lis[3]);
+// // Remove child Element
+// list.removeChild(lis[3]);
 
-// CLASSES & attribute
-const firstLi = document.querySelector('li:first-child');
-const link = firstLi.children[0];
+// // CLASSES & attribute
+// const firstLi = document.querySelector('li:first-child');
+// const link = firstLi.children[0];
 
-// Classes
-val = link.className;
-val = link.classList;
-val = link.classList[0];
-link.classList.add('test');
-link.classList.remove('test');
-val = link;
+// // Classes
+// val = link.className;
+// val = link.classList;
+// val = link.classList[0];
+// link.classList.add('test');
+// link.classList.remove('test');
+// val = link;
 
-// Attributes
-val = link.getAttribute('href');
-val = link.setAttribute('href', 'http://google.com');
-link.setAttribute('title', 'Google');
-val = link.hasAttribute('title');
-val = link;
-link.removeAttribute('title');
+// // Attributes
+// val = link.getAttribute('href');
+// val = link.setAttribute('href', 'http://google.com');
+// link.setAttribute('title', 'Google');
+// val = link.hasAttribute('title');
+// val = link;
+// link.removeAttribute('title');
 
-console.log(val);
-//console.log(firstLi.children[0]);
+// console.log(val);
+// //console.log(firstLi.children[0]);
+
+// document.querySelector('.clear-tasks').addEventListener('click',
+// function(e){
+//     console.log('Hello World');
+//     //e.preventDefault();
+// });
+
+document.querySelector('.clear-tasks').addEventListener('click',
+onclick);
+
+function onclick(e){
+    //console.log('clicked');
+
+val = e;
+
+    console.log(val);
+}
