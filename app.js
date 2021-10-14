b3d0203d0cd7dcd69174b96634b9afea2b7bec2e
kelechi prosper <prosperkelechi50@gmail.com>
2021-10-14 08:05:15 +0100

    i

--- app.js
+++ app.js
@@ -1058,19 +1058,25 @@ let val;
 
 const form = document.querySelector('form');
 const taskInput = document.getElementById('task');
+const heading = document.querySelector('h5');
 
 // clear input
 taskInput.value = '';
 
 //form.addEventListener('submit', runEvent);
-
-taskInput.addEventListener('keydown', runEvent);
+//Keydown
+//taskInput.addEventListener('keydown', runEvent);
+//Keyup
+taskInput.addEventListener('keyup', runEvent);
 
 
 function runEvent(e){
     console.log(`EVENT TYPE ${e.type}`);
 
     console.log(e.target.value);
+
+    //heading.innerText = e.target.value;
+
     //Get input value
 //     console.log(taskInput.value);
 
