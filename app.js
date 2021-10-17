59a76f21108b551a686e2f73ec62b798e64def4f
kelechi prosper <prosperkelechi50@gmail.com>
2021-10-17 10:22:25 +0100

    i

--- app.js
+++ app.js
@@ -1129,15 +1129,18 @@ let val;
 // const delItem = document.querySelector('.delete-item');
 // delItem.addEventListener('click',deleteItem);
 
-document.body.addEventListener('click', deleteItem);
+// document.body.addEventListener('click', deleteItem);
 
-function deleteItem(e){
-    // if(e.target.parentElement.className === 'delete-item secondary-content');
-    // console.log('delete item');
+// function deleteItem(e){
+//     // if(e.target.parentElement.className === 'delete-item secondary-content');
+//     // console.log('delete item');
     
-    if(e.target.parentElement.classList.contains('delete-item')){
-        console.log('delete item');
-        e.target.parentElement.parentElement.remove();
-    }
+//     if(e.target.parentElement.classList.contains('delete-item')){
+//         console.log('delete item');
+//         e.target.parentElement.parentElement.remove();
+//     }
     
-}
+// }
+
+// Set local storage item
+localStorage.setItem('name', 'John');
