7a9b49c43d7576a7811bf2c4e1084540e8bf9e66
kelechi prosper <prosperkelechi50@gmail.com>
2021-10-19 00:27:20 +0100

    i

--- app.js
+++ app.js
@@ -1164,8 +1164,24 @@ let val;
 document.querySelector('form').addEventListener('submit',
 function(e){
     const task = document.getElementById('task').value;
-    localStorage.setItem('task', task);
+
+    let tasks;
+
+    if(localStorage.getItem('tasks') ===null) {
+        tasks = [];
+    } else {
+        task = JSON.parse(localStorage.getItem('tasks'));
+    }
+    tasks.push(task);
+
+    localStorage.setItem('tasks', JSON.stringify(tasks));
     alert('Task saved')
 
     e.preventDefault();
 });
+
+const tasks = JSON.parse(localStorage.getItem('tasks'));
+
+tasks.forEach(function(task){
+    console.log(task);
+})
